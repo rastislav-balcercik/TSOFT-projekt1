2739ec5edc2d0ee55b7ad337e2c574e9fc1c82ea
Rastislav Balcerčík <rasto.balcercik@gmail.com>
2024-04-17 00:03:11 +0200

    TSOFT projekt1 - selenium e2e

---
+++ src/tests/textChange.test.js
@@ -0,0 +1,31 @@
+const chrome = require('selenium-webdriver/chrome');
+const {Builder, Browser, By } = require('selenium-webdriver');
+ 
+(async function textChangeText() {
+  const driver = await new Builder()
+    .forBrowser(Browser.CHROME)
+    .setChromeOptions(new chrome.Options().addArguments('--headless'))
+    .build();
+ 
+    try {
+    await driver.get('http://localhost:3000');
+    const text = await driver.findElement(By.id('text')).getText();
+
+    // Kontrola či sa text zobrazuje správne
+    if (text !== 'Rastislav Balcerčík') {
+      throw new Error('Initial text is not correct');
+    }
+
+    const button = await driver.findElement(By.id('button'));
+    await button.click();
+
+    // Kontrola či sa text zmenil
+    const newText = await driver.findElement(By.id('text')).getText();
+    if (newText !== 'RB') {
+      throw new Error('Text was not changed');
+    }
+  } finally {
+    await driver.quit();
+    console.log('Test passed');
+  }
+})();
